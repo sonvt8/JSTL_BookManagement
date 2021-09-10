fb89e3cb67c1eea2823842eb9ed1fd3892ecf180
Son Vu <ly.thl2505@sinhvien.hoasen.edu.vn>
2021-09-10 09:14:32 +0700

    create models

---
+++ src/main/java/com/tommy/models/Book.java
@@ -0,0 +1,80 @@
+package com.tommy.models;
+
+import java.util.Date;
+
+public class Book {
+	private int id;
+    private String title;
+    private String author;
+    private double price;
+    private int quantity;
+    private String description;
+    private Date released;
+    private int cateId;
+
+    public Book() { }
+    
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Date getReleased() {
+		return released;
+	}
+
+	public void setReleased(Date released) {
+		this.released = released;
+	}
+
+	public int getCateId() {
+		return cateId;
+	}
+
+	public void setCateId(int cateId) {
+		this.cateId = cateId;
+	}
+}
--- src/main/webapp/navbar.jsp
+++ src/main/webapp/navbar.jsp
@@ -1,4 +1,8 @@
-
+<%@ page contentType="text/html;charset=UTF-8" language="java"
+	import="com.tommy.dbmodels.*
+            , com.tommy.helpers.*
+            , java.sql.*"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
 <div class="navbar navbar-inverse bg-slate-700 navbar-component"
 	style="position: relative; z-index: 27;">
 	<div class="navbar-header">
@@ -26,11 +30,19 @@
 		</ul>
 
 		<ul class="nav navbar-nav navbar-right">
-			<li><a href="#"><i class="icon-file-locked position-left"></i>
-					Logout</a></li>
-			<p class="navbar-text">
-				Signed in as <a href="#" class="navbar-link">Eugene</a>
-			</p>
+			<c:choose>
+				<c:when test="${sessionScope.authorized_user != null}">
+					<li><a href="#"><i class="icon-exit position-left"></i>
+							Logout</a></li>
+					<p class="navbar-text">
+						Signed in as <a href="#" class="navbar-link">Eugene</a>
+					</p>
+				</c:when>
+				<c:otherwise>
+					<li><a href="index.jsp"><i class="icon-enter position-left"></i>
+							Login</a></li>
+				</c:otherwise>
+			</c:choose>
 		</ul>
 	</div>
 </div>
